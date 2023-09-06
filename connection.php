dc28c51eb4de96c7b251d1ee0f2cb85ea4429f75
brydres <bryanb1489@gmail.com>
2023-09-06 09:56:04 -0500

    corrigiendo

--- connection.php
+++ connection.php
@@ -5,21 +5,12 @@ $username = 'root';
 $password = '';
 $database = 'db_project_3';
 
-// Try to connect to the database
 try {
     $mysqli = new mysqli($host, $username, $password, $database);
-    // echo "Conectado a proyecto";
 } catch (mysqli_sql_exception $e) {
     die('Error de conexión: ' . $e->getMessage());
 }
 
-// Check if the connection was successful
 if ($mysqli->connect_error) {
     die('Error de conexión: ' . $mysqli->connect_error);
 }
-
-
-
-
-// Close the connection to the database
-//$mysqli->close();
